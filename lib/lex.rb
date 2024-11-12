80f3279197285572c9ccf48556611bce6ad7039e
shaquinbale <scopelenskingdra@gmail.com>
2024-11-12 15:40:59 -0500

    Add the lexer class and sketch out functions

---
+++ lib/lex.rb
@@ -0,0 +1,35 @@
+class Lexer
+  def initialize(source)
+    
+  end
+
+  # Process next character
+  def next_char
+    
+  end
+
+  # Return the lookahead character
+  def peek
+    
+  end
+
+  # Invalid token found, print error message and exit
+  def abort(message)
+    
+  end
+
+  # Skip whitespace, except for newlines, which indicate the end of the statement
+  def skip_whitespace
+    
+  end
+
+  # Skip comments in code
+  def skip_comments
+    
+  end
+
+  # Return next token
+  def get_token
+    
+  end
+end
