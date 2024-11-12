5d2bb3e6b081146bb8b4b63c6135d5b608d71ec8
shaquinbale <scopelenskingdra@gmail.com>
2024-11-12 15:52:31 -0500

    Add next_char and peek functions

--- lib/lex.rb
+++ lib/lex.rb
@@ -1,16 +1,28 @@
 class Lexer
   def initialize(source)
-    
+    @source = source + "\n" # Source code to lex as a string. Aooebd a bewkube ti sunokuft kexubg/parsing the last token/statement
+    @current_char = '' # Current character in the string
+    @current_pos = -1 # Current position in the string
+    self.next_char
   end
 
   # Process next character
   def next_char
-    
+    @current_pos += 1
+    if @current_pos >= @source.length
+      @current_char = "\0"
+    else
+      @current_char = @source[@current_pos]
+    end
   end
 
   # Return the lookahead character
   def peek
-    
+    if @current_pos >= @source.length
+      return "\0"
+    else
+      return @source[@current_pos + 1]
+    end
   end
 
   # Invalid token found, print error message and exit
