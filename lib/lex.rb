50c6a82f3eeaa49257f05e75953f2f476972a8ad
shaquinbale <scopelenskingdra@gmail.com>
2024-11-12 21:00:04 -0500

    Add token class, lexer can detect operators
    The lexer can parse basic arithmetic operators
    such as +, -, /, etc.
    
    Additionally, a token class and TokenType enum
    was added

--- lib/lex.rb
+++ lib/lex.rb
@@ -44,6 +44,64 @@ class Lexer
 
   # Return next token
   def get_token
-    
+    token = nil
+
+    if @current_char == '+'
+      token = Token.new(@current_char, TokenType::PLUS)
+    elsif @current_char == '-'
+      token = Token.new(@current_char, TokenType::MINUS)
+    elsif @current_char == '*'
+      token = Token.new(@current_char, TokenType::ASTERISK)
+    elsif @current_char == '/'
+      token = Token.new(@current_char, TokenType::SLASH)
+    elsif @current_char == "\n"
+      token = Token.new(@current_char, TokenType::NEWLINE)
+    elsif @current_char == "\0"
+      token = Token.new(@current_char, TokenType::EOF)
+    else
+      # Unkown Token!
+    end
+    self.next_char
   end
+end
+
+class Token
+  def initialize(token_text, token_kind)
+    @text = token_text
+    @king = token_kind
+  end
+end
+
+module TokenType
+  EOF = -1
+  NEWLINE = 0
+  NUMBER = 1
+  IDENT = 2
+  STRING = 3
+
+  # Keywords.
+  LABEL = 101
+  GOTO = 102
+  PRINT = 103
+  INPUT = 104
+  LET = 105
+  IF = 106
+  THEN = 107
+  ENDIF = 108
+  WHILE = 109
+  REPEAT = 110
+  ENDWHILE = 111
+
+  # Operators.
+  EQ = 201
+  PLUS = 202
+  MINUS = 203
+  ASTERISK = 204
+  SLASH = 205
+  EQEQ = 206
+  NOTEQ = 207
+  LT = 208
+  LTEQ = 209
+  GT = 210
+  GTEQ = 211
 end
